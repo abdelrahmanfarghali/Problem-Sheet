4bbe643ec05850e05b8019a10d2f0834218c3ec9
chakertriangle <36712666+chakertriangle@users.noreply.github.com>
2019-06-21 17:46:25 +0200

    Add files via upload
    progress in solving the hardest problem in sheet

--- 99E.cpp
+++ 99E.cpp
@@ -9,25 +9,26 @@ int main()
 	int n;
 	cin >> n;
 	int j = 0;
-	for(int y = 0; y < n; y++)
+	for(int y = 1; y < n; y++)
 	{
 		char tetrahedron[3] = {'A', 'B', 'C'};
-		for(int x = 0; x < n; x++)
-		{	
+		while(tetrahedron[0] != NULL || tetrahedron[1] != NULL || tetrahedron[2] != NULL)
+		{
 			int s = 0;
 			//DABaD, DACaD, DCBaD, DCAbD, DBAbD, DBCbD
 			//;DABcD, DACbD, DCBcD, DCAcD, DBAcD, DBCaD
 			//;DADAD, DBDAD, DCDAD, DADBD, DBDBD, DCDBD, DADCD, DBDCD, DCDCD
+			//hypnothesis about the first route can be varied from - to
+			//hypnothesis: n = 2 route = 1;n = 4 route = 6;n = 6 route = 4 * 21
+			//hypnothesis: route = 1 * 2, 2 * 3, 3 * 4, 4 * 5 n = 2, 4, 5, 6 respectively
 			s = rand() % n + 0;
 			if(tetrahedron[s] != NULL)
 			{
-				j++;
+				j += y;
 				tetrahedron[s] = NULL;
 			}
 		}
 	}
-
-
 	cout << j << endl;
 	/////////////////////////
 	clock_t end = clock();
